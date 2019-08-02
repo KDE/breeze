1fb03d56e95da852ce05685b7085aa49c6c71eca
Noah Davis <noahadvs@gmail.com>
2019-08-02 17:13:59 -0400

    Remove _config argument

--- kstyle/breezehelper.cpp
+++ kstyle/breezehelper.cpp
@@ -60,10 +60,10 @@ namespace Breeze
     //____________________________________________________________________
     void Helper::loadConfig()
     {
-        _viewFocusBrush = KStatefulBrush( KColorScheme::View, KColorScheme::FocusColor, _config );
-        _viewHoverBrush = KStatefulBrush( KColorScheme::View, KColorScheme::HoverColor, _config );
-        _viewNegativeTextBrush = KStatefulBrush( KColorScheme::View, KColorScheme::NegativeText, _config );
-        _selectionBackgroundBrush = KStatefulBrush( KColorScheme::Selection, KColorScheme::NormalBackground, _config );
+        _viewFocusBrush = KStatefulBrush( KColorScheme::View, KColorScheme::FocusColor );
+        _viewHoverBrush = KStatefulBrush( KColorScheme::View, KColorScheme::HoverColor );
+        _viewNegativeTextBrush = KStatefulBrush( KColorScheme::View, KColorScheme::NegativeText );
+        _selectionBackgroundBrush = KStatefulBrush( KColorScheme::Selection, KColorScheme::NormalBackground );
 
         const QPalette palette( QApplication::palette() );
         const KConfigGroup group( _config->group( "WM" ) );
