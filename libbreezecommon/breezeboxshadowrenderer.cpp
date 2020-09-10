ee55b18de658347f2e14d0485481e67ae96f86d8
David Redondo <kde@david-redondo.de>
2020-09-10 09:45:46 +0200

    Convert copyright statements to SPDX expressions
    I was in a video call with Volker and asked him under which license the GtkUpdateIconCache
    is licensed.

--- libbreezecommon/breezeboxshadowrenderer.cpp
+++ libbreezecommon/breezeboxshadowrenderer.cpp
@@ -1,12 +1,9 @@
 /*
- * Copyright (C) 2018 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+ * SPDX-FileCopyrightText: 2018 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
  *
  * The box blur implementation is based on AlphaBoxBlur from Firefox.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * SPDX-License-Identifier: GPL-2.0-or-later
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
