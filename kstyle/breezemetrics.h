7f18a88cb1e11630055aae52b8431f5ad8224137
Fabian Vogt <fabian@ritter-vogt.de>
2020-11-16 13:10:02 +0100

    Fix gap between control and frame
    7e678c34 removed the offsets from all sides, but that created a gap at the
    right and bottom edges. This is because the frame is actually just 1px on
    each side, but Frame_FrameWidth didn't match that.

--- kstyle/breezemetrics.h
+++ kstyle/breezemetrics.h
@@ -35,7 +35,7 @@ namespace Breeze
     struct Metrics
     {
         // frames
-        static constexpr int Frame_FrameWidth = 2;
+        static constexpr int Frame_FrameWidth = 1;
         static constexpr int Frame_FrameRadius = 3;
 
         // layout
