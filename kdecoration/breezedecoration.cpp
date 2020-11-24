c33f10bff840038af55a274777e83c095ec7f05f
SaucySauce <38703177+SaucySauce@users.noreply.github.com>
2020-11-24 21:36:14 -0100

    Removal of blue separator outline code.
    It doesn't fit the proposed styling of future breeze updates nor is it common to have it enabled.

--- kdecoration/breezedecoration.cpp
+++ kdecoration/breezedecoration.cpp
@@ -634,16 +634,6 @@ namespace Breeze
 
         }
 
-        const QColor outlineColor( this->outlineColor() );
-        if( !c->isShaded() && outlineColor.isValid() )
-        {
-            // outline
-            painter->setRenderHint( QPainter::Antialiasing, false );
-            painter->setBrush( Qt::NoBrush );
-            painter->setPen( outlineColor );
-            painter->drawLine( titleRect.bottomLeft(), titleRect.bottomRight() );
-        }
-
         painter->restore();
 
         // draw caption
