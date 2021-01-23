c3e2d0b777149d3393467565e869e53f47f1caaf
Jan Blackquill <uhhadd@gmail.com>
2021-01-23 17:59:44 -0500

    [kstyle]: Don't draw line when no toolbars are present in fullscreen
    Since there's no titlebar to draw a separator under, a separator just looks strange.
    
    BUG: 431969

--- kstyle/breezetoolsareamanager.cpp
+++ kstyle/breezetoolsareamanager.cpp
@@ -64,7 +64,7 @@ namespace Breeze {
             }
         }
 
-        if (itemHeight == 0 && !(window->property(PropertyNames::noSeparator).toBool())) {
+        if (itemHeight == 0 && !(window->property(PropertyNames::noSeparator).toBool()) && !window->isFullScreen()) {
             auto win = const_cast<QMainWindow*>(window);
             win->setContentsMargins(0, 1, 0, 0);
         } else {
