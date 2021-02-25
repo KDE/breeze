bf7c35808a3a29aff9359b44ab54d48426f95d25
Jan Blackquill <uhhadd@gmail.com>
2021-02-25 22:39:07 +0000

    Revert "[kstyle]: Add double-ringed focus for text fields"
    This reverts commit 2f351fe101d6a706b618b4f9518581006f50b242.
    
    We hve not yet figured out how to apply this style consistently
    and were unable to implement it for controls other than text fields
    in Plasma 5.21, so it is better to remove it for now than to have an
    inconsistent UI. We will re-evaluate this for Plasma 5.22.
    
    BUG: 430944
    BUG: 430943
    BUG: 433421
    FIXED-IN: 5.21.2
    
    
    (cherry picked from commit f9758726eeff08212778ea04c1fc284989ef2e82)

--- kstyle/breezestyle.h
+++ kstyle/breezestyle.h
@@ -234,7 +234,6 @@ namespace Breeze
         { return true; }
 
         bool drawFramePrimitive( const QStyleOption*, QPainter*, const QWidget* ) const;
-        bool drawPanelLineEditPrimitive( const QStyleOption*, QPainter*, const QWidget* ) const;
         bool drawFrameLineEditPrimitive( const QStyleOption*, QPainter*, const QWidget* ) const;
         bool drawFrameFocusRectPrimitive( const QStyleOption*, QPainter*, const QWidget* ) const;
         bool drawFrameMenuPrimitive( const QStyleOption*, QPainter*, const QWidget* ) const;
