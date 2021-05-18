KDE/breeze

commit d201a1f187243da677f8fa6e94325c41c3b9007c

Fix SplitterProxy not clearing when above another QSplitterHandle
When two SplitterHandles are next to each other, like at the intersection of a
horizontal and vertical splitter (|-), then it's possible that hiding the proxy
of one of those handles causes the other handle to gain focus immediately,
which activates the SplitterProxy again. Before this patch, it would then
continue clearing after reenabling itself, leading to an inconsistent state.

BUG: 431921

kstyle/breezesplitterproxy.cpp
@@ -338,11 +338,6 @@ namespace Breeze
         // release mouse
         if( mouseGrabber() == this ) releaseMouse();
 
-        // hide
-        parentWidget()->setUpdatesEnabled(false);
-        hide();
-        parentWidget()->setUpdatesEnabled(true);
-
         // send hover event
         if( _splitter )
         {
@@ -351,7 +346,6 @@ namespace Breeze
                 _splitter.data()->mapFromGlobal(QCursor::pos()), _hook);
             QCoreApplication::sendEvent( _splitter.data(), &hoverEvent );
             _splitter.clear();
-
         }
 
         // kill timer if any
@@ -361,6 +355,12 @@ namespace Breeze
             _timerId = 0;
         }
 
+        // hide
+        parentWidget()->setUpdatesEnabled(false);
+        // Note: This sends a synthetic mouse event to the widget below (to get focus), which might be
+        // another SplitterHandle, therefore enabling this SplitterProxy again!
+        hide();
+        parentWidget()->setUpdatesEnabled(true);
     }
 
 }

commit f99b7ef621c9c69544158d245699fd8104db6753

Fix informing the underlying widget when leaving SplitterProxy
While the SplitterProxy is active, it intercepts all relevant events, so that
the underlying widget still thinks it's in the same "on splitter" state. When
the SplitterProxy is left, the underlying widget is sent a HoverLeave/HoverMove
event to make it aware of the new current cursor position. Without this, it
doesn't know that it's not supposed to be in the "on splitter" state, and when
it regains focus it just re-activates the SplitterProxy at the current cursor
position.

This was broken by accident in d201a1f187 ("Fix SplitterProxy not clearing
when above another QSplitterHandle"), which moved the hide() call past the
call to QCoreApplication::sendEvent. Previously that made isVisible() false,
which also prevented the interception of the HoverLeave/HoverMove events.

BUG: 436473

kstyle/breezesplitterproxy.cpp
@@ -341,11 +341,14 @@ namespace Breeze
         // send hover event
         if( _splitter )
         {
-            QHoverEvent hoverEvent(
-                qobject_cast<QSplitterHandle*>(_splitter.data()) ? QEvent::HoverLeave : QEvent::HoverMove,
-                _splitter.data()->mapFromGlobal(QCursor::pos()), _hook);
-            QCoreApplication::sendEvent( _splitter.data(), &hoverEvent );
+            // SplitterProxy intercepts HoverLeave/HoverMove events to _splitter,
+            // but this is meant to reach it directly. Unset _splitter to stop interception.
+            auto splitter = _splitter;
             _splitter.clear();
+            QHoverEvent hoverEvent(
+                qobject_cast<QSplitterHandle*>(splitter.data()) ? QEvent::HoverLeave : QEvent::HoverMove,
+                splitter.data()->mapFromGlobal(QCursor::pos()), _hook);
+            QCoreApplication::sendEvent( splitter.data(), &hoverEvent );
         }
 
         // kill timer if any