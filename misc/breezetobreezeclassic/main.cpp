84d0d258e0d84b0a294761a932e5a4132faad1ac
Nate Graham <nate@kde.org>
2021-09-21 10:51:35 -0600

    Rename Breeze to Breeze Classic
    This is no longer the default color scheme; Breeze Light is. But being
    named "Breeze" implies that is is the default or at least the base,
    rather than simply one of several variants. Let's rename it to "Breeze
    Classic" to make it follow the pattern and avoid subtly giving it more
    textual weight.

---
+++ misc/breezetobreezeclassic/main.cpp
@@ -0,0 +1,13 @@
+#include <KConfig>
+#include <KConfigGroup>
+
+int main()
+{
+    KConfig globals("kdeglobals");
+    KConfigGroup general(&globals, "General");
+    if (general.readEntry(QStringLiteral("ColorScheme")) != QStringLiteral("Breeze")) {
+        return 0;
+    }
+    general.writeEntry(QStringLiteral("ColorScheme"), QStringLiteral("BreezeClassic"));
+    // No need to migrate the serialized colors because they're the same
+}
