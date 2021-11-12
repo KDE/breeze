90f34ac84d79558dfda994874e0775617e92b8d5
Paul McAuley <kde@paulmcauley.com>
2021-11-12 01:50:50 +0000

    Make kstyle aware of Full-sized Rounded Rectange button shape

--- kstyle/breezehelper.cpp
+++ kstyle/breezehelper.cpp
@@ -1408,7 +1408,10 @@ namespace Breeze
             if( inverted ) painter->setBrush( color );
             else painter->setBrush( backgroundColor );
             
-            if ( decorationConfig()->buttonShape() ==  InternalSettings::EnumButtonShape::ShapeFullSizedRectangle || decorationConfig()->buttonShape() ==  InternalSettings::EnumButtonShape::ShapeSquare)
+            if ( decorationConfig()->buttonShape() ==  InternalSettings::EnumButtonShape::ShapeFullSizedRectangle
+                || decorationConfig()->buttonShape() ==  InternalSettings::EnumButtonShape::ShapeSquare
+                || decorationConfig()->buttonShape() ==  InternalSettings::EnumButtonShape::ShapeFullSizedRoundedRectangle
+            )
                 painter->drawRoundedRect( QRectF( 2, 2, 14, 14 ), 20, 20, Qt::RelativeSize );
             else painter->drawEllipse( QRectF( 0, 0, 18, 18 ) );
             
