9caf38d23c236d727b8aba580697756c1505785c
Volker Krause <vkrause@kde.org>
2021-12-31 18:28:24 +0100

    Switch from QRegExp to QRegularExpression

--- kdecoration/config/breezeexceptionlistwidget.cpp
+++ kdecoration/config/breezeexceptionlistwidget.cpp
@@ -15,6 +15,7 @@
 #include <QMessageBox>
 #include <QPointer>
 #include <QIcon>
+#include <QRegularExpression>
 
 //__________________________________________________________
 namespace Breeze
@@ -307,7 +308,7 @@ namespace Breeze
     bool ExceptionListWidget::checkException( InternalSettingsPtr exception )
     {
 
-        while( exception->exceptionPattern().isEmpty() || !QRegExp( exception->exceptionPattern() ).isValid() )
+        while( exception->exceptionPattern().isEmpty() || !QRegularExpression( exception->exceptionPattern() ).isValid() )
         {
 
             QMessageBox::warning( this, i18n( "Warning - Breeze Settings" ), i18n("Regular Expression syntax is incorrect") );
