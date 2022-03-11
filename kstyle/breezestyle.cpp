af1ddf45ed7ba611ac2d03209ad1d24b0d6d33a6
Jan Blackquill <uhhadd@gmail.com>
2022-03-11 14:08:24 -0500

    kstyle: don't show states for frames with bordersSides
    Showing focus/hover/etc. often looks strange/inconsistent without frames
    on all sides, so if we're drawing on frames other than all sides, don't
    show those states. It's also more consistent with the style we're trying
    to emulate, which doesn't show focus/hover on separator lines between
    views.

--- kstyle/breezestyle.cpp
+++ kstyle/breezestyle.cpp
@@ -3280,7 +3280,7 @@ namespace Breeze
         {
 
             const auto background( palette.base().color() );
-            const auto outline( _helper->frameOutlineColor( palette, mouseOver, hasFocus, opacity, mode ) );
+            const auto outline( _helper->frameOutlineColor( palette ) );
             _helper->renderFrameWithSides( painter, rect, background, widget->property( PropertyNames::bordersSides ).value<Qt::Edges>(), outline );
 
             return true;
