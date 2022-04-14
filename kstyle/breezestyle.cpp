37e44cf31a11631e19d63dfc02773789b346b372
Janet Blackquill <uhhadd@gmail.com>
2022-04-14 15:36:13 -0400

    kstyle: make inactive windows more visibly inactive
    - buttons now flatten
    - default buttons don't show their blue colour, because, they aren't the
      button that'll get triggered on enter if their window isn't active

--- kstyle/breezestyle.cpp
+++ kstyle/breezestyle.cpp
@@ -3862,6 +3862,7 @@ namespace Breeze
         stateProperties["hasMenu"] = hasMenu;
         stateProperties["defaultButton"] = defaultButton;
         stateProperties["hasNeutralHighlight"] = hasNeutralHighlight;
+        stateProperties["isActiveWindow"] = widget ? widget->isActiveWindow() : true;
 
         _helper->renderButtonFrame(painter, option->rect, option->palette, stateProperties, bgAnimation, penAnimation);
 
