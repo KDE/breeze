110c590313ceaaacafe69e24a21c080bd27b7574
Friedrich W. H. Kossebau <kossebau@kde.org>
2022-05-29 00:31:56 +0200

    Fix mixup of PM_ToolBarItemMargin & PM_ToolBarFrameWidth
    While Breeze draws no frames around toolbars, yet it had the style hint
    for the frame width set to 6, while having the actual toolbar margin
    set to 0.
    
    The possible cause is that PM_ToolBarItemMargin was only introduced for
    Qt4, so seemingly Qt3 times' workaround to set the margin via the frame
    width instead of having more of the CSS box model now available missed
    to be adapted to in the minds of the style developers.
    
    Qt's own codee uses PM_ToolBarItemMargin & PM_ToolBarFrameWidth to
    calculate the QToolbar layout's contentmargin, so switching the values
    results in no actual change there.
    The only other use is PM_ToolBarFrameWidth for initializing the
    QStyleOptionToolBar::lineWidth, which though is unused by Oxygen drawing
    code, so the change also has no effect.
    
    But 3rd-party code querying the pixel metric for these two values after
    this patch gets proper values and can properly align custom UI painting with the
    styled toolbar.

--- kstyle/breezemetrics.h
+++ kstyle/breezemetrics.h
@@ -102,11 +102,12 @@ static int const &ScrollBar_SingleButtonHeight = ScrollBar_Extend + ScrollBar_To
 static int const &ScrollBar_DoubleButtonHeight = 2 * ScrollBar_Extend + ScrollBar_TopBottomMargins;
 
 // toolbars
-static constexpr int ToolBar_FrameWidth = 6;
+static constexpr int ToolBar_FrameWidth = 0;
 static constexpr int ToolBar_HandleExtent = 10;
 static constexpr int ToolBar_HandleWidth = 6;
 static constexpr int ToolBar_SeparatorWidth = 8;
 static constexpr int ToolBar_ExtensionWidth = 20;
+static constexpr int ToolBar_ItemMargin = 6;
 static constexpr int ToolBar_ItemSpacing = 0;
 
 // progressbars
