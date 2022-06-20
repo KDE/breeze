bd99a3482f481508cf9e76fd723036e544ad4909
Ingo Klöcker <dev@ingo-kloecker.de>
2022-06-20 14:48:13 +0200

    kstyle: Fix build with KF 5.90
    The minimum required KF is 5.90, but the namespaced include of
    TabletModeWatcher didn't exist until KF 5.91. Use the old non-namespaced
    include, if the namespaced include doesn't exist.

--- kstyle/breezestyle.cpp
+++ kstyle/breezestyle.cpp
@@ -56,7 +56,12 @@
 
 #if BREEZE_HAVE_QTQUICK
 #include <KCoreAddons>
+#if __has_include(<Kirigami/TabletModeWatcher>)
+// the namespaced include is new in KF 5.91
 #include <Kirigami/TabletModeWatcher>
+#else
+#include <TabletModeWatcher>
+#endif
 #include <QQuickWindow>
 #endif
 
