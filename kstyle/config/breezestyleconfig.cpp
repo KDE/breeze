57f091541f449065858e952d3d56a983cf01d783
Paul McAuley <kde@paulmcauley.com>
2022-07-09 04:22:17 +0100

    clang-format changes after gradient effect option

--- kstyle/config/breezestyleconfig.cpp
+++ kstyle/config/breezestyleconfig.cpp
@@ -49,7 +49,7 @@ StyleConfig::StyleConfig(QWidget *parent)
     load();
 
     connect(_tabBarDrawCenteredTabs, &QAbstractButton::toggled, this, &StyleConfig::updateChanged);
-    connect( _buttonGradient, &QAbstractButton::toggled, this, &StyleConfig::updateChanged );
+    connect(_buttonGradient, &QAbstractButton::toggled, this, &StyleConfig::updateChanged);
     connect(_toolBarDrawItemSeparator, &QAbstractButton::toggled, this, &StyleConfig::updateChanged);
     connect(_viewDrawFocusIndicator, &QAbstractButton::toggled, this, &StyleConfig::updateChanged);
     connect(_dockWidgetDrawFrame, &QAbstractButton::toggled, this, &StyleConfig::updateChanged);
@@ -83,7 +83,7 @@ StyleConfig::StyleConfig(QWidget *parent)
 void StyleConfig::save()
 {
     StyleConfigData::setTabBarDrawCenteredTabs(_tabBarDrawCenteredTabs->isChecked());
-    StyleConfigData::setButtonGradient( _buttonGradient->isChecked() );
+    StyleConfigData::setButtonGradient(_buttonGradient->isChecked());
     StyleConfigData::setToolBarDrawItemSeparator(_toolBarDrawItemSeparator->isChecked());
     StyleConfigData::setViewDrawFocusIndicator(_viewDrawFocusIndicator->isChecked());
     StyleConfigData::setDockWidgetDrawFrame(_dockWidgetDrawFrame->isChecked());
@@ -139,7 +139,7 @@ void StyleConfig::updateChanged()
     // check if any value was modified
     if (_tabBarDrawCenteredTabs->isChecked() != StyleConfigData::tabBarDrawCenteredTabs())
         modified = true;
-    else if( _buttonGradient->isChecked() != StyleConfigData::buttonGradient() )
+    else if (_buttonGradient->isChecked() != StyleConfigData::buttonGradient())
         modified = true;
     else if (_toolBarDrawItemSeparator->isChecked() != StyleConfigData::toolBarDrawItemSeparator())
         modified = true;
@@ -193,7 +193,7 @@ void StyleConfig::updateChanged()
 void StyleConfig::load()
 {
     _tabBarDrawCenteredTabs->setChecked(StyleConfigData::tabBarDrawCenteredTabs());
-    _buttonGradient->setChecked( StyleConfigData::buttonGradient() );
+    _buttonGradient->setChecked(StyleConfigData::buttonGradient());
     _toolBarDrawItemSeparator->setChecked(StyleConfigData::toolBarDrawItemSeparator());
     _viewDrawFocusIndicator->setChecked(StyleConfigData::viewDrawFocusIndicator());
     _dockWidgetDrawFrame->setChecked(StyleConfigData::dockWidgetDrawFrame());
