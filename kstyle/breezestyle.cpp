ffe65ffce072b967757bcbb024214537e9de3bcb
Paul McAuley <kde@paulmcauley.com>
2022-07-25 01:08:55 +0100

    Fix segfault if no widget is present

--- kstyle/breezestyle.cpp
+++ kstyle/breezestyle.cpp
@@ -7814,7 +7814,13 @@ QIcon Style::titleBarButtonIcon(StandardPixmap standardPixmap, const QStyleOptio
 
     // output icon
     QIcon icon;
-
+    
+    qreal devicePixelRatio;
+    if(widget)
+        devicePixelRatio = widget->devicePixelRatio();
+    else
+        devicePixelRatio = 1;
+    
     foreach (const IconData &iconData, iconTypes) {
         foreach (const int &iconSize, iconSizes) {
             // create pixmap
@@ -7831,7 +7837,7 @@ QIcon Style::titleBarButtonIcon(StandardPixmap standardPixmap, const QStyleOptio
                                             iconData._paintBackground,
                                             iconData._backgroundColor,
                                             iconData._outlineColor,
-                                            widget->devicePixelRatioF());
+                                            devicePixelRatio);
 
             painter.end();
 
