e44ed33d3f12fd979a6da1a085dca7e9c93ccc7d
Fushan Wen <qydwhotmail@gmail.com>
2022-10-05 17:43:29 +0800

    wallpapers: use all available CPU cores to generate wallpapers
    After dark variant is added, the script needs to generate more
    wallpapers. This uses all the available CPU cores to generate
    wallpapers, which should shorten the generating time on modern
    computers.
    
    Before:
    > time python3.10 ./generate_wallpaper_sizes.py
    
    real    2m26.763s
    user    2m26.208s
    sys     0m0.353s
    
    After:
    > time python3.10 ./generate_wallpaper_sizes.py
    
    real    0m38.969s
    user    2m53.784s
    sys     0m2.547s

--- wallpapers/generate_wallpaper_sizes.py
+++ wallpapers/generate_wallpaper_sizes.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import logging
+from multiprocessing import cpu_count
+from multiprocessing.pool import Pool
 from pathlib import Path
 from itertools import chain
 from typing import Final
@@ -30,25 +32,42 @@ templates = {
 
 PIL_VERSION: Final = tuple(map(int, PIL.__version__.split(".")))
 
+
+def resize_and_save_image(file: Path, image: Image, width: int, height: int) -> None:
+	"""
+	Image.LANCZOS is deprecated since 9.1.0 https://pillow.readthedocs.io/en/stable/deprecations.html#constants
+	"""
+	logging.info(f'Generating {width}x{height}')
+
+	base_dir, extension = file.parent, file.suffix
+	base_width, base_height = image.size
+
+	if width / height > base_width / base_height:
+		crop = int(base_height - height / (width / base_width)) // 2
+		box = (0, crop, base_width, base_height - crop)
+	elif width / height < base_width / base_height:
+		crop = int(base_width - width / (height / base_height)) // 2
+		box = (crop, 0, base_width - crop, base_height)
+	else:
+		box = None
+
+	if PIL_VERSION >= (9, 1):
+		resized_image = image.resize((width, height), Image.Resampling.LANCZOS, box)
+	else:
+		resized_image = image.resize((width, height), Image.LANCZOS, box)
+
+	resized_image.save(base_dir / f'{width}x{height}{extension}',
+					  quality=90, optimize=True, subsampling=1)
+
+
+argument_list: list[tuple] = []
+
 for orientation in ('horizontal', 'vertical'):
 	for file in chain(*map(Path().rglob, templates[orientation])):
 		image = Image.open(file)
-		image_path = Path(file)
-		base_dir, extension = image_path.parent, image_path.suffix
-		base_width, base_height = image.size
+		image.load();
 		for width, height in sizes[orientation]:
-			logging.info(f'Generating {width}x{height}')
-			if width/height > base_width/base_height:
-				crop = int(base_height - height/(width/base_width))//2
-				box = (0, crop, base_width, base_height-crop)
-			elif width/height < base_width/base_height:
-				crop = int(base_width - width/(height/base_height))//2
-				box = (crop, 0, base_width-crop, base_height)
-			else: box = None
-			# Image.LANCZOS is deprecated since 9.1.0 https://pillow.readthedocs.io/en/stable/deprecations.html#constants
-			if PIL_VERSION >= (9, 1):
-				resized_image = image.resize((width, height), Image.Resampling.LANCZOS, box)
-			else:
-				resized_image = image.resize((width, height), Image.LANCZOS, box)
-			resized_image.save(base_dir / f'{width}x{height}{extension}',
-					  quality=90, optimize=True, subsampling=1)
+			argument_list.append((file, image, width, height))
+
+with Pool(processes=cpu_count()) as pool:
+	pool.starmap(resize_and_save_image, argument_list)
