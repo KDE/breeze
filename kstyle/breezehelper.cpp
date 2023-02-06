3040e8d6f6b901137400a4d267f90953899bdf85
ivan tkachenko <me@ratijas.tk>
2023-02-06 12:49:38 +0300

    Revert "MenuItem: Don't stretch icons, instead scale them down to fit rect"
    This reverts commit c320472b60a04dce8fa347c49095fc0dab1fbfbf.
    
    It was an older version of the patch, accidentally committed from the
    wrong (KF6) copy of breeze repo. Oops!
    
    (cherry picked from commit 7ddbd935c7a24688a242fda9f89eb91fbb5d0138)

--- kstyle/breezehelper.cpp
+++ kstyle/breezehelper.cpp
@@ -1617,8 +1617,7 @@ QPixmap Helper::coloredIcon(const QIcon &icon, const QPalette &palette, const QS
     if (changePalette) {
         KIconLoader::global()->setCustomPalette(palette);
     }
-    const QSize actualSize = icon.actualSize(size, mode, state);
-    const QPixmap pixmap = icon.pixmap(actualSize, mode, state);
+    const QPixmap pixmap = icon.pixmap(size, mode, state);
     if (changePalette) {
         if (activePalette == QPalette()) {
             KIconLoader::global()->resetPalette();
