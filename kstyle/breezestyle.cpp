4d03b8a19b8460411f0f49b7b6a438fc57b91e16
Carl Schwan <carl@carlschwan.eu>
2024-01-06 01:50:17 +0100

    Use new QPalette::Accent color for small UI element
    Instead of using QPalette::Highlight for both large background areas
    like in list item and menu items and small areas like outline and small
    ui elements, use QPalette::Accent for small UI elements and
    QPalette::Highlight for large UI elements.
    
    Currently both color are the same but in the future this will allow us
    to use a lighter color for focused item delegates while keeping the
    strong blue as accent color for smaller elements.

--- kstyle/breezestyle.cpp
+++ kstyle/breezestyle.cpp
@@ -81,6 +81,12 @@ using TabletModeWatcher = Kirigami::TabletModeWatcher;
 
 #include "breeze_logging.h"
 
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+constexpr auto HighlightColor = QPalette::Accent;
+#else
+constexpr auto HighlightColor = QPalette::Highlight;
+#endif
+
 namespace BreezePrivate
 {
 
@@ -161,7 +167,7 @@ public:
         if (option.showDecorationSelected && (option.state & QStyle::State_Selected)) {
             using namespace Breeze;
 
-            auto c = option.palette.brush((option.state & QStyle::State_Enabled) ? QPalette::Normal : QPalette::Disabled, QPalette::Highlight).color();
+            auto c = option.palette.brush((option.state & QStyle::State_Enabled) ? QPalette::Normal : QPalette::Disabled, HighlightColor).color();
 
             painter->setPen(c);
             c.setAlphaF(c.alphaF() * 0.3);
@@ -4333,7 +4339,7 @@ bool Style::drawFrameFocusRectPrimitive(const QStyleOption *option, QPainter *pa
         return true;
     }
 
-    const auto outlineColor(state & State_Selected ? palette.color(QPalette::HighlightedText) : palette.color(QPalette::Highlight));
+    const auto outlineColor(state & State_Selected ? palette.color(QPalette::HighlightedText) : palette.color(HighlightColor));
     painter->setRenderHint(QPainter::Antialiasing, false);
     painter->setPen(outlineColor);
     painter->drawLine(QPoint(rect.bottomLeft() - QPoint(0, 1)), QPoint(rect.bottomRight() - QPoint(0, 1)));
@@ -6270,8 +6276,8 @@ bool Style::drawProgressBarContentsControl(const QStyleOption *option, QPainter 
     if (busy) {
         const int progress(_animations->busyIndicatorEngine().value());
 
-        const auto &first = palette.color(QPalette::Highlight);
-        const auto second(KColorUtils::mix(palette.color(QPalette::Highlight), palette.color(QPalette::Window), 0.7));
+        const auto &first = palette.color(HighlightColor);
+        const auto second(KColorUtils::mix(palette.color(HighlightColor), palette.color(QPalette::Window), 0.7));
         _helper->renderProgressBarBusyContents(painter, rect, first, second, horizontal, reverse, progress);
 
     } else {
@@ -6297,7 +6303,7 @@ bool Style::drawProgressBarContentsControl(const QStyleOption *option, QPainter 
             }
         }
 
-        auto contentsColor(option->state.testFlag(QStyle::State_Selected) ? palette.color(QPalette::HighlightedText) : palette.color(QPalette::Highlight));
+        auto contentsColor(option->state.testFlag(QStyle::State_Selected) ? palette.color(QPalette::HighlightedText) : palette.color(HighlightColor));
 
         _helper->renderProgressBarGroove(painter, rect, contentsColor, palette.color(QPalette::Window));
         painter->setClipRegion(oldClipRegion);
@@ -6806,8 +6812,8 @@ bool Style::drawRubberBandControl(const QStyleOption *option, QPainter *painter,
     painter->setRenderHints(QPainter::Antialiasing);
 
     const auto &palette(option->palette);
-    const auto outline = KColorUtils::lighten(palette.color(QPalette::Highlight));
-    auto background = palette.color(QPalette::Highlight);
+    const auto outline = KColorUtils::lighten(palette.color(HighlightColor));
+    auto background = palette.color(HighlightColor);
     background.setAlphaF(0.20);
 
     painter->setPen(outline);
@@ -7903,8 +7909,7 @@ bool Style::drawSliderComplexControl(const QStyleOptionComplex *option, QPainter
             // colors
             const auto reverse(option->direction == Qt::RightToLeft);
             const auto base(_helper->separatorColor(palette));
-            const auto &highlight =
-                hasHighlightNeutral(widget, option, mouseOver, hasFocus) ? _helper->neutralText(palette) : palette.color(QPalette::Highlight);
+            const auto &highlight = hasHighlightNeutral(widget, option, mouseOver, hasFocus) ? _helper->neutralText(palette) : palette.color(HighlightColor);
 
             while (current <= sliderOption->maximum) {
                 // adjust color
@@ -7938,8 +7943,7 @@ bool Style::drawSliderComplexControl(const QStyleOptionComplex *option, QPainter
             const bool upsideDown(sliderOption->upsideDown);
 
             // highlight color
-            const auto &highlight =
-                hasHighlightNeutral(widget, option, mouseOver, hasFocus) ? _helper->neutralText(palette) : palette.color(QPalette::Highlight);
+            const auto &highlight = hasHighlightNeutral(widget, option, mouseOver, hasFocus) ? _helper->neutralText(palette) : palette.color(HighlightColor);
 
             if (sliderOption->orientation == Qt::Horizontal) {
                 auto leftRect(grooveRect);
@@ -8035,7 +8039,7 @@ bool Style::drawDialComplexControl(const QStyleOptionComplex *option, QPainter *
 
         if (enabled) {
             // highlight
-            const auto &highlight = palette.color(QPalette::Highlight);
+            const auto &highlight = palette.color(HighlightColor);
 
             // angles
             const qreal second(dialAngle(sliderOption, sliderOption->sliderPosition));
