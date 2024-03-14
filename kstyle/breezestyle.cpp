c949bb4bd288bba5eda06e9a89ea0a054920c719
Kai Uwe Broulik <kde@privat.broulik.de>
2024-03-14 23:06:29 +0100

    kstyle: Don't send event to disabled scroll bar
    If nobody accepts the event, it can lead to infinite recursion.
    
    BUG: 483487
    
    
    (cherry picked from commit 8af48c52063d04883c055ba3cf0f5a2ddefef41c)

--- kstyle/breezestyle.cpp
+++ kstyle/breezestyle.cpp
@@ -1752,7 +1752,7 @@ bool Style::eventFilterScrollArea(QWidget *widget, QEvent *event)
 
         // loop over found scrollbars
         for (QScrollBar *scrollBar : std::as_const(scrollBars)) {
-            if (!(scrollBar && scrollBar->isVisible())) {
+            if (!(scrollBar && scrollBar->isVisible() && scrollBar->isEnabled())) {
                 continue;
             }
 
