c51fd7bef15853ed8fb601dae0a47ebc80cefbdb
Martin Rys <martin@rys.pw>
2024-03-30 15:32:23 +0000

    Major script optimization and fixes
    inkscape was being launched HUNDREDS of times, launch it just twice per theme now.
    
    Took build times from 200s+ to some 7s~ on my machine.

--- cursors/Breeze/build.sh
+++ cursors/Breeze/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+
 # Open initial output.
 # Prefer konsole if its there, otherwise fall back to xterminal.
 #tty -s; if [ $? -ne 0 ]; then
@@ -16,22 +18,22 @@ ALIASES="src/cursorList"
 
 
 echo -ne "Checking Requirements...\\r"
-if [ ! -f $RAWSVG ] ; then
-	echo -e "\\nFAIL: '$RAWSVG' missing in /src"
+if [[ ! -f "${RAWSVG}" ]]; then
+	echo -e "\\nFAIL: '${RAWSVG}' missing in /src"
 	exit 1
 fi
 
-if [ ! -f $INDEX ] ; then
-	echo -e "\\nFAIL: '$INDEX' missing in /src"
+if [[ ! -f "${INDEX}" ]]; then
+	echo -e "\\nFAIL: '${INDEX}' missing in /src"
 	exit 1
 fi
 
-if  ! type "inkscape" > /dev/null ; then
+if ! command -v inkscape > /dev/null ; then
 	echo -e "\\nFAIL: inkscape must be installed"
 	exit 1
 fi
 
-if  ! type "xcursorgen" > /dev/null ; then
+if ! command -v xcursorgen > /dev/null ; then
 	echo -e "\\nFAIL: xcursorgen must be installed"
 	exit 1
 fi
@@ -39,7 +41,7 @@ echo -e "Checking Requirements... DONE"
 
 SCALES="50 75 100 125 150 175 200 225 250 275 300"
 
-echo -ne "Making Folders... $BASENAME\\r"
+echo -ne "Making Folders... \\r"
 OUTPUT="$(grep --only-matching --perl-regex "(?<=Name\=).*$" $INDEX)"
 OUTPUT=${OUTPUT// /_}
 for scale in $SCALES; do
@@ -48,63 +50,56 @@ done
 mkdir -p "$OUTPUT/cursors"
 echo 'Making Folders... DONE';
 
-
-
+echo -ne "Generating simple cursor pixmaps...\\r"
+genPixmaps="file-open:${RAWSVG};"
 for CUR in src/config/*.cursor; do
-	BASENAME=$CUR
-	BASENAME=${BASENAME##*/}
+	BASENAME=${CUR##*/}
 	BASENAME=${BASENAME%.*}
 
-	echo -ne "\033[0KGenerating simple cursor pixmaps... $BASENAME\\r"
 
 	for scale in $SCALES; do
-		DIR="build/x$scale"
-		if [ "$DIR/$BASENAME.png" -ot $RAWSVG ] ; then
-			inkscape $RAWSVG -i $BASENAME -w $((32*$scale/100)) -h $((32*$scale/100)) -o "$DIR/$BASENAME.png" > /dev/null
+		DIR="build/x${scale}"
+		if [[ "${DIR}/${BASENAME}.png" -ot ${RAWSVG} ]]; then
+			genPixmaps="${genPixmaps} export-id:${BASENAME}; export-width:$((32*scale/100)); export-height:$((32*scale/100)); export-filename:${DIR}/${BASENAME}.png; export-do;"
 		fi
 	done
 done
+inkscape --shell < <(echo "${genPixmaps}") > /dev/null
 echo -e "\033[0KGenerating simple cursor pixmaps... DONE"
 
-
-
-for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23
-do
-	echo -ne "\033[0KGenerating animated cursor pixmaps... $i / 23 \\r"
-
+echo -ne "\033[0KGenerating animated cursor pixmaps... \\r"
+genPixmaps="file-open:${RAWSVG};"
+for i in {01..23}; do
 	for scale in $SCALES; do
 		DIR="build/x$scale"
-		
-		if [ "$DIR/progress-$i.png" -ot $RAWSVG ] ; then
-			inkscape $RAWSVG -i progress-$i -w $((32*$scale/100)) -h $((32*$scale/100)) -o "$DIR/progress-$i.png" > /dev/null
+
+		if [[ "$DIR/progress-$i.png" -ot $RAWSVG ]]; then
+			genPixmaps="${genPixmaps} export-id:progress-${i}; export-width:$((32*scale/100)); export-height:$((32*scale/100)); export-filename:${DIR}/progress-${i}.png; export-do;"
 		fi
 
-		if [ "$DIR/wait-$i.png" -ot $RAWSVG ] ; then
-			inkscape $RAWSVG -i wait-$i -w $((32*$scale/100)) -h $((32*$scale/100)) -o "$DIR/wait-$i.png" > /dev/null
+		if [[ "$DIR/wait-$i.png" -ot $RAWSVG ]]; then
+			genPixmaps="${genPixmaps} export-id:wait-${i}; export-width:$((32*scale/100)); export-height:$((32*scale/100)); export-filename:${DIR}/wait-${i}.png; export-do;"
 		fi
 	done
 done
+inkscape --shell < <(echo "${genPixmaps}") > /dev/null
 echo -e "\033[0KGenerating animated cursor pixmaps... DONE"
-
 
 
 echo -ne "Generating cursor theme...\\r"
 for CUR in src/config/*.cursor; do
-	BASENAME=$CUR
-	BASENAME=${BASENAME##*/}
+	BASENAME=${CUR##*/}
 	BASENAME=${BASENAME%.*}
 
 	TMP_CUR="tmp.cursor"
-	./scale_cursor $CUR $SCALES > $TMP_CUR
-	ERR="$( xcursorgen -p build "$TMP_CUR" "$OUTPUT/cursors/$BASENAME" 2>&1 )"
-	rm $TMP_CUR
-
-	if [[ "$?" -ne "0" ]]; then
+	./scale_cursor "${CUR}" ${SCALES} > $TMP_CUR
+	if ! ERR="$( xcursorgen -p build "$TMP_CUR" "$OUTPUT/cursors/$BASENAME" 2>&1 )"; then
 		echo "FAIL: $CUR $ERR"
+		rm -f "${TMP_CUR}"
 	fi
+	rm -f "${TMP_CUR}"
 done
 echo -e "Generating cursor theme... DONE"
-
 
 
 echo -ne "Generating shortcuts...\\r"
@@ -112,7 +107,7 @@ while read ALIAS ; do
 	FROM=${ALIAS% *}
 	TO=${ALIAS#* }
 
-	if [ -e "$OUTPUT/cursors/$FROM" ] ; then
+	if [[ -e "$OUTPUT/cursors/$FROM" ]]; then
 		continue
 	fi
 
@@ -120,14 +115,10 @@ while read ALIAS ; do
 done < $ALIASES
 echo -e "\033[0KGenerating shortcuts... DONE"
 
-
-
 echo -ne "Copying Theme Index...\\r"
-	if ! [ -e "$OUTPUT/$INDEX" ] ; then
+	if ! [[ -e "$OUTPUT/$INDEX" ]]; then
 		cp $INDEX "$OUTPUT/index.theme"
 	fi
 echo -e "\033[0KCopying Theme Index... DONE"
-
-
 
 echo "COMPLETE!"
